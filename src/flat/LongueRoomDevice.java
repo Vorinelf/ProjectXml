f3b4f72ee9b44894c07ed6db50dfdf6db599cdda
Vorinelf <44244914+Vorinelf@users.noreply.github.com>
2019-01-03 23:23:44 +0300

    ProjectXML2

--- src/flat/LongueRoomDevice.java
+++ src/flat/LongueRoomDevice.java
@@ -1,41 +1,41 @@
 package flat;
 
 public class LongueRoomDevice extends Device {
-    private boolean LCD_display;
-    private boolean wi_fi;
+    private boolean lcdDisplay;
+    private boolean wiFi;
     private boolean bluetooth;
 
     public LongueRoomDevice () {}
 
-    public LongueRoomDevice(String name, int powerSize_kW, boolean powerON, boolean lcd_display, boolean wi_fi, boolean bluetooth) {
-        super(name, powerSize_kW, powerON);
-        LCD_display = lcd_display;
-        this.wi_fi = wi_fi;
+    public LongueRoomDevice(String name, int powerSizekW, boolean powerON, boolean lcdDisplay, boolean wiFi, boolean bluetooth) {
+        super(name, powerSizekW, powerON);
+        lcdDisplay = lcdDisplay;
+        this.wiFi = wiFi;
         this.bluetooth = bluetooth;
     }
 
-    public void setLCD_display(boolean LCD_display) {
-        this.LCD_display = LCD_display;
+    public boolean isLcdDisplay() {
+        return lcdDisplay;
     }
 
-    public void setWi_fi(boolean wi_fi) {
-        this.wi_fi = wi_fi;
+    public void setLcdDisplay(boolean lcdDisplay) {
+        this.lcdDisplay = lcdDisplay;
     }
 
-    public void setBluetooth(boolean bluetooth) {
-        this.bluetooth = bluetooth;
+    public boolean isWiFi() {
+        return wiFi;
     }
 
-    public boolean isLCD_display() {
-        return LCD_display;
-    }
-
-    public boolean isWi_fi() {
-        return wi_fi;
+    public void setWiFi(boolean wiFi) {
+        this.wiFi = wiFi;
     }
 
     public boolean isBluetooth() {
         return bluetooth;
+    }
+
+    public void setBluetooth(boolean bluetooth) {
+        this.bluetooth = bluetooth;
     }
 }
 
